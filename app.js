124e284c54c39fd9fc729153592feeb50032e832
Tony <tonychenkt@gmail.com>
2013-05-03 02:57:24 -0700

    stylus bullshit

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 // The App
 var express = require("express");
+var routes = require('./routes');
+var stylus = require("stylus");
+var nib= require("nib");
 var app = express();
 // Use static middleware
 app.use(express.static(__dirname + '/public'));
 // Set the view engine
 app.set('view engine', 'jade');
+
+//stylus
+app.use(stylus.middleware({
+  src: __dirname + '/public/stylesheets',
+  compile: function(str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .set('compress', true)
+      .use(nib());
+  }
+}));
 
 // Set the directory that contains the views
 app.set('views', __dirname + '/views');
@@ -21,10 +35,11 @@ app.use(function (req, res, next) {
 });
 // Use the router middleware
 app.use(app.router);
+
 // Get route with one middleware
-app.get("/", function (req, res) {
-  res.send("root");
-});
+// configure routes
+app.get("/", routes.index);
+
 // Create HTTP server with your app
 var http = require("http");
 var server = http.createServer(app)
